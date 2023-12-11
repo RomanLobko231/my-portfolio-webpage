a39beda14dd015e465cbe2d26078c68095f519bb
Roman <roomen817@gmail.com>
2023-12-11 15:59:40 +0100

    added screenshots

--- src/skillset/index.js
+++ src/skillset/index.js
@@ -1,17 +1,17 @@
 export const canSkills = [
-    { name: 'Java', src: '/java.png' },
-    { name: 'Kotlin', src: '/kotlin_logo.png' },
-    { name: 'Spring Boot', src: '/springboot_logo.png' },
-    { name: 'Spring Data JPA', src: '/spring_data1.png' },
-    { name: 'PostgreSQL', src: '/postgresql_logo.png' },
-    { name: 'GitHub', src: '/github_logo.png' }
+    { name: 'Java', src: '/skills_logos/java.png' },
+    { name: 'Kotlin', src: '/skills_logos/kotlin_logo.png' },
+    { name: 'Spring Boot', src: '/skills_logos/springboot_logo.png' },
+    { name: 'Spring Data JPA', src: '/skills_logos/spring_data1.png' },
+    { name: 'PostgreSQL', src: '/skills_logos/postgresql_logo.png' },
+    { name: 'GitHub', src: '/skills_logos/github_logo.png' }
 ]
 
 export const familiarSkills = [    
-    { name: 'Docker', src: '/docker_logo.png' },
-    { name: 'Spring Security', src: '/spring_security2.png' },
-    { name: 'ReactJS', src: '/reactjs_logo.png' },
-    { name: 'CSS', src: '/css_logo.png' },
-    { name: 'HTML', src: '/html_logo.png' },
-    { name: 'MySQL', src: '/mysql_logo.png' }
+    { name: 'Docker', src: '/skills_logos/docker_logo.png' },
+    { name: 'Spring Security', src: '/skills_logos/spring_security2.png' },
+    { name: 'ReactJS', src: '/skills_logos/reactjs_logo.png' },
+    { name: 'CSS', src: '/skills_logos/css_logo.png' },
+    { name: 'HTML', src: '/skills_logos/html_logo.png' },
+    { name: 'MySQL', src: '/skills_logos/mysql_logo.png' }
 ]
