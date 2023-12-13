d2951c0b393fad35c2efbd819ebba8e9740d8005
Roman <roomen817@gmail.com>
2023-12-13 15:56:28 +0100

    b

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,10 +1,11 @@
 import GreetingsComponent from "../UI/greetings/GreetingsComponent";
+import SkillsetComponent from "../UI/skills/skillset/SkillsetComponent";
 
 const Main = () => {
   return (
     <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
       <GreetingsComponent />
-      {/* <SkillsetComponent /> */}
+      <SkillsetComponent />
     </div>
 
   );
