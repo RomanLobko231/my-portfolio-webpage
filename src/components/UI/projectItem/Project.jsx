e592a07dab4fec69fb050f4e2db6335bbe8594f5
Roman <roomen817@gmail.com>
2023-12-13 18:45:47 +0100

    commit

--- src/components/UI/projectItem/Project.jsx
+++ src/components/UI/projectItem/Project.jsx
@@ -1,21 +1,17 @@
 import Carousel from "../carousel/Carousel";
+import cl from "./Project.module.css"
 
 const Project = ({project}) => {
 
     return (
        
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                gap: '20px'
-            }}>
-                <div style={{ width: '20%', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '16px'}}>
+            <div className={cl.container}>
+                <div className={cl.project__title}>
                 <h1 style={{color: '#574c4c'}}>{project.name}</h1>
                 {project.link !== '' && <a target='_blank' rel='noopener noreferrer' href={project.link}>#link to github</a>}
                 </div>
                 <Carousel images={project.src} />
-                <p  style={{width: '60%', fontSize: 'larger'}}>{project.description}</p>
+                <p className={cl.project__description}>{project.description}</p>
             </div>
     );
 };
