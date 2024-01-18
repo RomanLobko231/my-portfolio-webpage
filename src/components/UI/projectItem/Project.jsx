68d66311bb6a0618ca335898ea1d02275db087ee
Roman <roomen817@gmail.com>
2024-01-19 00:06:23 +0100

    added a project

--- src/components/UI/projectItem/Project.jsx
+++ src/components/UI/projectItem/Project.jsx
@@ -4,13 +4,12 @@ import cl from "./Project.module.css"
 const Project = ({project}) => {
 
     return (
-       
             <div className={cl.container}>
                 <div className={cl.project__title}>
                 <h1 style={{color: '#574c4c'}}>{project.name}</h1>
                 {project.link !== '' && <a target='_blank' rel='noopener noreferrer' href={project.link}>#link to github</a>}
                 </div>
-                <Carousel images={project.src} />
+                {project.src && <Carousel images={project.src} />}
                 <p style={{whiteSpace: 'pre-line'}}className={cl.project__description}>{project.description}</p>
             </div>
     );
