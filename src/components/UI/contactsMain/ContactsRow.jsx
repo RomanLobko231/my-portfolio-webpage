5164223fe59b10422115cf120f6d7bc2bd44be1f
Roman <roomen817@gmail.com>
2024-02-07 00:18:55 +0100

    c

--- src/components/UI/contactsMain/ContactsRow.jsx
+++ src/components/UI/contactsMain/ContactsRow.jsx
@@ -1,6 +1,6 @@
 import cl from "./ContactsRow.module.css"
 import { DiGithubBadge } from "react-icons/di";
-import { BiLogoGmail } from "react-icons/bi";
+import { BiLogoGmail, BiLogoLinkedinSquare } from "react-icons/bi";
 import { MdLocalPhone } from "react-icons/md";
 
 const ContactsRow = () => {
@@ -11,14 +11,17 @@ const ContactsRow = () => {
                 <h3>roomen817@gmail.com</h3>
             </div>
             <div className={cl.contacts__item}>
+                <MdLocalPhone/>
+                <h3>+4793944025</h3>
+            </div>
+            <div className={cl.contacts__item}>
                 <DiGithubBadge />
                 <a target='_blank' rel='noopener noreferrer' href="https://github.com/RomanLobko231">Github</a>
             </div >
             <div className={cl.contacts__item}>
-                <MdLocalPhone/>
-                <h3>+4793944025</h3>
-            </div>
-
+                <BiLogoLinkedinSquare />
+                <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/roman-lobko-dev">LinkedIn</a>
+            </div >
 
         </div>
     );
