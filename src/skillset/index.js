9f710cafd5230b728abfb2be05631a16b0ba038b
Roman <roomen817@gmail.com>
2024-02-20 21:05:33 +0100

    c

--- src/skillset/index.js
+++ src/skillset/index.js
@@ -3,15 +3,17 @@ export const canSkills = [
     { name: 'Kotlin', src: '/skills_logos/kotlin_logo.png' },
     { name: 'Spring Boot', src: '/skills_logos/springboot_logo.png' },
     { name: 'Spring Data JPA', src: '/skills_logos/spring_data1.png' },
+    { name: 'Spring Security', src: '/skills_logos/spring_security2.png' },
     { name: 'PostgreSQL', src: '/skills_logos/postgresql_logo.png' },
+    { name: 'Flyway', src: '/skills_logos/flyway_logo.png' },
     { name: 'GitHub', src: '/skills_logos/github_logo.png' }
 ]
 
 export const familiarSkills = [    
     { name: 'Docker', src: '/skills_logos/docker_logo.png' },
-    { name: 'Spring Security', src: '/skills_logos/spring_security2.png' },
     { name: 'ReactJS', src: '/skills_logos/reactjs_logo.png' },
     { name: 'CSS', src: '/skills_logos/css_logo.png' },
     { name: 'HTML', src: '/skills_logos/html_logo.png' },
-    { name: 'MySQL', src: '/skills_logos/mysql_logo.png' }
+    { name: 'MySQL', src: '/skills_logos/mysql_logo.png' },
+    { name: 'Thymeleaf', src: '/skills_logos/thymeleaf_logo.png' },
 ]
